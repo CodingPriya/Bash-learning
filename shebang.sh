c41ef305787c9b6994ecb3f3857cca47603dc4b5
ArunPaleti407 <99018689+ArunPaleti407@users.noreply.github.com>
2026-05-10 17:29:45 +0530

    Bash-learning

---
+++ shebang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# First line should always start with shebang notation!
+# Shebang notation always starts with "#!"
+# In linux, apart from Shebang notation, anything that starts with # is a comment, that means that's just for human understanding purpose only
+
+# How do I execute this script ?
+# bash scriptName
+
+echo "Hello World"
+
