7a9cdb315a415924ce91560567f6557d0113a9db
ArunPaleti407 <99018689+ArunPaleti407@users.noreply.github.com>
2026-05-10 19:24:04 +0530

    Bash-learning

--- color.sh
+++ color.sh
@@ -5,4 +5,4 @@ echo -e "\e[33m I am printing yellow color \e[0m"
 echo -e "\e[34m I am printing blue color \e[0m"
 echo -e "\e[35m I am printing magenta color \e[0m"
 
-echo -e "\e[43;31m Printing red on yellow\e[0m]"
+echo -e "\e[43;31m Printing red on yellow\e[0m"
