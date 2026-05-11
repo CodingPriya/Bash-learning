0562f32425958e9cc46155997eae53a35c1d6eff
CodingPriya <priyadarshinim97@gmail.com>
2026-05-12 00:17:12 +0530

    Bash-learning

---
+++ 8functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+stat() {
+    echo "Beginnng of the stat function"
+    echo "Todays date is : $(date +%f)"
+    echo "Number of logged in sessions : $(who | wc -l)"
+    echo "stat function completed"
+    
+}
+
