af4280f98f7933111db18b5c5299744a1b768b9e
ArunPaleti407 <99018689+ArunPaleti407@users.noreply.github.com>
2026-05-11 09:39:54 +0530

    Bash-learning

--- 5specialVariables.sh
+++ 5specialVariables.sh
@@ -7,3 +7,4 @@ echo "I am printing the first argument : $firstVar"
 echo "I am printing the second argument : $secondVar"
 
 echo "Name of the executed script is : $0"
+echo "Arguments used in the scripts are : $*"
