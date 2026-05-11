42b8e6c083743f27627f8e6215809ce7599c426d
CodingPriya <priyadarshinim97@gmail.com>
2026-05-11 11:52:29 +0530

    Bash-learning

--- 7read.sh
+++ 7read.sh
@@ -3,3 +3,5 @@
 echo "This script will take the input from the user by interrupting the execeution"
 read -p "Please enter your name : " name
 echo "User who executed the script is : $name"
+echo "Above is the user who executed the script"
+echo "Name of the script is : $0"
